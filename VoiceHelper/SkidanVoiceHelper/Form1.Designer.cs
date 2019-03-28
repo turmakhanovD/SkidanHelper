f4dfa1ede0cfd4edc162ab314157bdce95f158c1
Islam <denizzz9998882@gmail.com>
2019-03-28 11:55:51 +0600

    Voice HelPeR SkIdAn OlEg

---
+++ VoiceHelper/SkidanVoiceHelper/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SkidanVoiceHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(443, 407);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "Ирина";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.Color.White;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(150, 70);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 5000;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Black;
+            this.circularProgressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 54.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.White;
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.Black;
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(0, 0);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.Cyan;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.White;
+            this.circularProgressBar1.ProgressWidth = 25;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.circularProgressBar1.Size = new System.Drawing.Size(443, 407);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 1;
+            this.circularProgressBar1.Text = "Скидан О.";
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.circularProgressBar1.Value = 68;
+            this.circularProgressBar1.Click += new System.EventHandler(this.circularProgressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 407);
+            this.Controls.Add(this.circularProgressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Скидан О.";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+    }
+}
+
---
+++ VoiceHelper/SkidanVoiceHelper/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Globalization;
+using Microsoft.Speech.Recognition;
+using System.Reflection;
+
+
+namespace SkidanVoiceHelper
+{
+    public partial class Form1 : Form
+    {
+        private CultureInfo _culture;
+        private SpeechRecognitionEngine _sre;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1 = new NotifyIcon();
+
+            notifyIcon1.Visible = true;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetAutorunValue(true, Assembly.GetExecutingAssembly().Location);
+
+            try
+            {
+                _culture = new CultureInfo("ru-RU");
+
+                _sre = new SpeechRecognitionEngine(_culture);
+
+                // Setup event handlers
+                _sre.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(sr_SpeechDetected);
+                _sre.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(sr_RecognizeCompleted);
+                _sre.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(sr_SpeechHypothesized);
+                _sre.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(sr_SpeechRecognitionRejected);
+                _sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sr_SpeechRecognized);
+
+                // select input source
+                _sre.SetInputToDefaultAudioDevice();
+
+                // load grammar
+                _sre.LoadGrammar(CreateSampleGrammar1());
+                _sre.LoadGrammar(CreateSampleGrammar2());
+
+                // start recognition
+                _sre.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+        
+        private Choices CreateSampleChoices()
+        {
+            var val1 = new SemanticResultValue("калькулятор", "calc");
+            var val2 = new SemanticResultValue("проводник", "explorer");
+            var val3 = new SemanticResultValue("блокнот", "notepad");
+            var val4 = new SemanticResultValue("пэйнт", "mspaint");
+            var va15 = new SemanticResultValue("командную строку", "cmd");
+            var va16 = new SemanticResultValue("яндекс", "https://yandex.ru");
+            var va17 = new SemanticResultValue("гугл", "www.google.ru");
+            var va18 = new SemanticResultValue("вконтакте", "https://vk.com/");
+            var va19 = new SemanticResultValue("однокласники", "https://ok.ru");
+            var va20 = new SemanticResultValue("ютуп", "www.youtube.com");
+            var va21 = new SemanticResultValue("ворд", @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft Word 2010.lnk");
+            var va22 = new SemanticResultValue("эксэль", @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft Excel 2010.lnk");
+            var va23 = new SemanticResultValue("поврпоинт", @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft PowerPoint 2010.lnk");
+            var va24 = new SemanticResultValue("эксес", @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office\Microsoft Access 2010.lnk");
+            var va25 = new SemanticResultValue("подключение к удаленному рабочему столу", "mstsc");
+            var va26 = new SemanticResultValue("проигрыватель", "wmplayer");
+            var va27 = new SemanticResultValue("таблицу символов", "charmap");
+            var va28 = new SemanticResultValue("экранную лупу", "Magnify");
+            var va29 = new SemanticResultValue("диспетчер задач", "Taskmgr");
+            var va30 = new SemanticResultValue("редактор реестра", "regedit");
+            var va31 = new SemanticResultValue("визуальную студию", "devenv");
+            var va32 = new SemanticResultValue("матрицу", @"C:\Users\Acer\Desktop\Матрица\матрица.bat");
+            var va33 = new SemanticResultValue("доту", @"steam://rungameid/570");
+
+            return new Choices(val1, val2, val3, val4, va15, va16, va17, va18, va19, va20, va21, va22, va23, va24, va25, va26, va27, va28, va29, va30, va31, va32, va33);
+        }
+        
+        private Grammar CreateSampleGrammar1()
+        {
+            var programs = CreateSampleChoices();
+
+            var grammarBuilder = new GrammarBuilder("запустить", SubsetMatchingMode.SubsequenceContentRequired);
+            grammarBuilder.Culture = _culture;
+            grammarBuilder.Append(new SemanticResultKey("start", programs));
+
+            return new Grammar(grammarBuilder);
+            
+            
+        }
+
+        private Grammar CreateSampleGrammar2()
+        {
+            var programs = CreateSampleChoices();
+
+            var grammarBuilder = new GrammarBuilder("закрыть", SubsetMatchingMode.SubsequenceContentRequired);
+            grammarBuilder.Culture = _culture;
+            grammarBuilder.Append(new SemanticResultKey("close", programs));
+
+            return new Grammar(grammarBuilder);
+
+        }
+       
+
+        private void sr_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            AppendLine("Speech Recognition Rejected: " + e.Result.Text);
+        }
+
+        private void sr_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            AppendLine("Speech Hypothesized: " + e.Result.Text + " (" + e.Result.Confidence + ")");
+        }
+
+        private void sr_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        {
+            AppendLine("Recognize Completed: " + e.Result.Text);
+        }
+
+        private void sr_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            AppendLine("Speech Detected: audio pos " + e.AudioPosition);
+        }
+
+        private void sr_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            AppendLine("\t" + "Speech Recognized:");
+
+            AppendLine(e.Result.Text + " (" + e.Result.Confidence + ")");
+
+            if (e.Result.Confidence < 0.1f)
+                return;
+
+            for (var i = 0; i < e.Result.Alternates.Count; ++i)
+            {
+                AppendLine("\t" + "Alternate: " + e.Result.Alternates[i].Text + " (" + e.Result.Alternates[i].Confidence + ")");
+            }
+
+            for (var i = 0; i < e.Result.Words.Count; ++i)
+            {
+                AppendLine("\t" + "Word: " + e.Result.Words[i].Text + " (" + e.Result.Words[i].Confidence + ")");
+
+                if (e.Result.Words[i].Confidence < 0.1f)
+                    return;
+            }
+
+            foreach (var s in e.Result.Semantics)
+            {
+                var program = (string)s.Value.Value;
+
+                switch (s.Key)
+                {
+                    case "start":
+                        Process.Start(program);
+                        break;
+                    case "close":
+                        var p = Process.GetProcessesByName(program);
+                        if (p.Length > 0)
+                            p[0].Kill();
+                        break;
+                }
+            }
+        }
+
+        private void AppendLine(string text)
+        {
+            richTextBox1.AppendText(text + Environment.NewLine);
+            richTextBox1.ScrollToCaret();
+        }
+
+
+
+        
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private bool closing = true;
+
+       
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = closing;
+            this.Hide();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            closing = false;
+            Application.Exit();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Для того чтобы начать работать с программой необходимо знать 2 ключевых слова «Запустить» для запуска какой либо программы и «Закрыть» для закрытия программы. Например «Запустить Яндекс» или «Закрыть Яндекс».", "О программе");
+        }
+//автозагрузка
+        public bool SetAutorunValue(bool autorun, string path)
+        {
+            const string name = "Вова";
+            string ExePath = path;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                    reg.SetValue(name, ExePath);
+                else
+                    reg.DeleteValue(name);
+
+                reg.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void circularProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //
+    }
+}
+        
